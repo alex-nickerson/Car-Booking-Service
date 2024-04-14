74bd1bfca9a7d0496f3163fa89db33f5d1648d99
Alex Nickerson <nickersonaj@gmail.com>
2024-04-14 17:32:06 -0400

    Added another image slider, making 2 in total. Added an image to the image preview at top of page.

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-// IMAGE SLIDER
+// IMAGE SLIDERS
 
 const slides = document.querySelectorAll(".slides img");
+const slidesTwo = document.querySelectorAll(".slidesTwo img");
 let slideIndex = 0;
+let slideIndexTwo = 0;
 let intervalId = null;
+let intervalIdTwo = null;
 
 document.addEventListener("DOMContentLoaded", initializeSlider);
 
@@ -10,6 +13,10 @@ function initializeSlider(){
     if(slides.length > 0){
         slides[slideIndex].classList.add("displaySlide");
         intervalId = setInterval(nextSlide, 5000);
+    }
+    if(slidesTwo.length > 0){
+        slidesTwo[slideIndex].classList.add("displaySlide");
+        intervalIdTwo = setInterval(nextSlideTwo, 5000);
     }
 }
 
@@ -27,6 +34,20 @@ function showSlide(index){
     slides[slideIndex].classList.add("displaySlide");
 }
 
+function showSlideTwo(index){
+    if(index >= slidesTwo.length){
+        slideIndexTwo = 0;
+    }
+    else if(index < 0){
+        slideIndexTwo = slidesTwo.length - 1;
+    }
+
+    slidesTwo.forEach(slide => {
+        slide.classList.remove("displaySlide");
+    });
+    slidesTwo[slideIndexTwo].classList.add("displaySlide");
+}
+
 function prevSlide(){
     clearInterval(intervalId);
     slideIndex--;
@@ -36,4 +57,15 @@ function prevSlide(){
 function nextSlide(){
     slideIndex++;
     showSlide(slideIndex);
+}
+
+function prevSlideTwo(){
+    clearInterval(intervalIdTwo);
+    slideIndexTwo--;
+    showSlideTwo(slideIndexTwo);
+}
+
+function nextSlideTwo(){
+    slideIndexTwo++;
+    showSlideTwo(slideIndexTwo);
 }
